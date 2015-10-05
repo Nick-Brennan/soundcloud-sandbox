b2149e8190fa291a7627a82066e923d6ae492121
Nick Brennan <nick_p_b@yahoo.com>
2015-10-04 17:12:26 -0700

    added genre search, started duration search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,13 @@
 class WelcomeController < ApplicationController
 	require 'SoundCloud'
 
+	@@genre = 'magic taco'
+
 	def index
 		track_arr = []
 		names_arr = []
 		client = SoundCloud.new(client_id: 'b61acae9ab94159d1de902fdee787599')
-		tracks = client.get('/tracks?', :limit => 15, :genres => 'indie')
+		tracks = client.get('/tracks', :genres => @@genre)
 		puts tracks[1]
 		tracks.each do |track|
 			puts track.streamable
@@ -18,4 +20,10 @@ class WelcomeController < ApplicationController
 		@tracks = track_arr
 		@names = names_arr
 	end
+
+	def search
+		@@genre = params[:genre]
+		redirect_to '/'
+	end
+
 end
